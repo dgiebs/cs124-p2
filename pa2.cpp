a186440a099b86738a35860e23a4dc48d88c45da
dgiebs <dgiebisch@gmail.com>
2017-03-20 13:38:27 -0400

    ey

---
+++ pa2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <time.h>
+#include <cmath>
+#include <numeric>
+#include <stdio.h>
+#include <stdlib.h>
+#include "min_heap.h"
+
+using namespace std;
+
+int [] standard()
+
+int main( int argc, char *argv[])
+{
