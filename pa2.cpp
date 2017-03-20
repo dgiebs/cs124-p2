45fa66a8c71b9461d88b303187fc6b6625340bf0
dgiebs <dgiebisch@gmail.com>
2017-03-20 14:15:14 -0400

    ey

--- pa2.cpp
+++ pa2.cpp
@@ -18,10 +18,15 @@ int main( int argc, char *argv[])
 int* conventional(int* a, int* b, n){
 	int [n**2] value;
 
-	for (i = 0; i < n; i++){
-		for (j = 0; j < n; j = j++){
-			for (k = 0; k < n; k++)
-				value[i] = 0
+	for (i = 0; i < n^2; i += n){
+		for (j = 0; j < n; j++){
+			tmp = 0
+			for (k = 0; k < n; k++ ){// second matrix iteration
+				for (l = 0; l < n^2; l += n){ 
+					tmp += a[i + j] * b[k + l];
+				}
+			}
+			value [i + j] = tmp
 		}
 	}
 }
