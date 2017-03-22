78d9fae7c1bf29ad88e0f6376712439237a4122b
dgiebs <dgiebisch@gmail.com>
2017-03-22 10:30:48 -0400

    conventional

--- pa2.cpp
+++ pa2.cpp
@@ -16,17 +16,17 @@ int main( int argc, char *argv[])
 }
 
 int* conventional(int* a, int* b, n){
-	int [n**2] value;
+	int [pow(n, 2)] value;
 
-	for (i = 0; i < n^2; i += n){
+	for (i = 0; i < pow(n, 2); i += n){
 		for (j = 0; j < n; j++){
 			tmp = 0
 			for (k = 0; k < n; k++ ){// second matrix iteration
-				for (l = 0; l < n^2; l += n){ 
-					tmp += a[i + j] * b[k + l];
-				}
+				tmp += a[i + k] * b[k*n + j];
 			}
 			value [i + j] = tmp
 		}
 	}
 }
+
+int* strassens
