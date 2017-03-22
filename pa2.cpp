a66dc66db2a21ecb94ab81c925015b73f84f65b4
janetchen1 <janetchen@college.harvard.edu>
2017-03-22 09:46:51 -0400

    pseudocode/framework

--- pa2.cpp
+++ pa2.cpp
@@ -8,25 +8,48 @@
 
 using namespace std;
 
-int* conventional(int*, int*);
+#define STR_BASE 8
+
+int* conventional(int*, int*, int);
+int* strassens(int*, int*, int);
+pair<int*, int> matrixify(char* infile);
 
 int main( int argc, char *argv[])
 {
+	if (argc != 4){
+		printf("Incorrect number of parameters\n");
+		return 0;
+	}
 
+	int dimension = atoi(argv[2]);
+	inputfile = argv[3];
+}
+
+// convert file to matrix w d = 2^k
+pair<int*, int> matrixfiy(char* inputfile){
+	ifstream infile(inputfile);
+	string str;
+	while (getline(infile, str)){
+		// fill in matrix
+	}
 }
 
 int* conventional(int* a, int* b, n){
 	int [n**2] value;
 
-	for (i = 0; i < n^2; i += n){
+	for (i = 0; i < n**2; i += n){
 		for (j = 0; j < n; j++){
 			tmp = 0
 			for (k = 0; k < n; k++ ){// second matrix iteration
-				for (l = 0; l < n^2; l += n){ 
+				for (l = 0; l < n^2; l += n){
 					tmp += a[i + j] * b[k + l];
 				}
 			}
 			value [i + j] = tmp
 		}
 	}
+}
+
+int* strassens(int* a, int* b, n){
+
 }
