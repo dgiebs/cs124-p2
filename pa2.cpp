98e7fdf8ed3da91ca08d871b53c0fdd325600b1a
dgiebs <dgiebisch@gmail.com>
2017-03-22 10:50:12 -0400

    alg

--- pa2.cpp
+++ pa2.cpp
@@ -73,15 +73,13 @@ tuple<int*, int*, int> matrixfiy(char* inputfile, int dimension){
 }
 
 int* conventional(int* a, int* b, n){
-	int [n**2] value;
+	int [pow(n, 2)] value;
 
-	for (i = 0; i < n**2; i += n){
+	for (i = 0; i < pow(n, 2); i += n){
 		for (j = 0; j < n; j++){
 			tmp = 0
 			for (k = 0; k < n; k++ ){// second matrix iteration
-				for (l = 0; l < n^2; l += n){
-					tmp += a[i + j] * b[k + l];
-				}
+				tmp += a[i + k] * b[k*n + j];
 			}
 			value [i + j] = tmp
 		}
